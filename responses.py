e514ee17cd1aac6b13e785f46ca9cbe912ac32f5
Tim Hajdinjak <69644152+timhaj@users.noreply.github.com>
2024-04-22 19:05:06 +0200

    Added /help command

--- responses.py
+++ responses.py
@@ -4,6 +4,9 @@ from citati import vegova, fri
 def handle_response(message) -> str:
     p_message = message.lower()
 
+    if p_message == '/help':
+        return f'```fix\n/vegova``` -> vrne random vegova quote\n```fix\n/fri``` -> vrne random fri quote'
+
     if p_message == 'ok':
         return f'```fix\nNe ni ok\n ~ Katja Lasbaher ~```'
 
@@ -18,6 +21,6 @@ def handle_response(message) -> str:
         izbran = choice(fri)
         return f'```fix\n{izbran[0]}\n ~ {izbran[1]} ~```'        
 
-    return 'Uporab: /vegova'
+    return 'Uporab tole poba: /help'
 
 
