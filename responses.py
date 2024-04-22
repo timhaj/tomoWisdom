f72e1f8ff37f00e14c166681352e200be2c0224a
Tim Hajdinjak <69644152+timhaj@users.noreply.github.com>
2024-04-22 11:26:17 +0200

    Reformatted the reponse

--- responses.py
+++ responses.py
@@ -1,11 +1,16 @@
+from random import choice
+
+d = [
+    ("Glava pred roko", "Tomo Omahna"),
+    ("S katere hoste pa si ti pršu?", "Tomo Omahna")
+]
+
 def handle_response(message) -> str:
     p_message = message.lower()
 
-    if p_message == 'hello':
-        return 'S katere hoste pa si ti prsu?'
-
     if p_message == '/wisdom':
-        return 'Glava pred roko'
+        izbran = choice(d)
+        return f'```fix\n{izbran[0]}\n ~ {izbran[1]} ~```'
 
     return 'Za koksi kupil suknjič? Po dobavni ceni'
 
